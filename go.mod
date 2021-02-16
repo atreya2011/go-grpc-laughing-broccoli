7b752ff677a765ce6cf8337ba483e545c5f7ae57
Atreya <atreya2011@gmail.com>
2021-02-16 22:08:50 +0900

    Initial draft implementation using grpc-web

--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.15
 
 require (
 	github.com/bufbuild/buf v0.37.0
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.2.0
+	github.com/improbable-eng/grpc-web v0.14.0
+	github.com/jhump/protoreflect v1.8.2 // indirect
 	github.com/rakyll/statik v0.1.7
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/twitchtv/twirp v7.1.1+incompatible // indirect
 	google.golang.org/genproto v0.0.0-20210212180131-e7f2df4ecc2d
@@ -16,4 +20,5 @@ require (
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
 	google.golang.org/grpc/examples v0.0.0-20210212222430-425d405f3928 // indirect
 	google.golang.org/protobuf v1.25.1-0.20201208041424-160c7477e0e8
+	nhooyr.io/websocket v1.8.6 // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 
-	"github.com/atreya2011/go-grpc-laughing-broccoli/gateway"
 	pbExample "github.com/atreya2011/go-grpc-laughing-broccoli/proto"
 	"github.com/atreya2011/go-grpc-laughing-broccoli/server"
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 
 	// Static files
 	_ "github.com/atreya2011/go-grpc-laughing-broccoli/statik"
@@ -30,6 +31,13 @@ func main() {
 
 	s := grpc.NewServer(grpc.UnaryInterceptor(grpc_auth.UnaryServerInterceptor(server.ExampleAuthFunc)))
 
+	wrappedGrpc := grpcweb.WrapServer(s, grpcweb.WithOriginFunc(func(origin string) bool {
+		return origin == "http://localhost:3005"
+	}))
+	grpcWebHandler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		wrappedGrpc.ServeHTTP(resp, req)
+	})
+
 	pbExample.RegisterUserServiceServer(s, server.New())
 
 	// Serve gRPC Server
@@ -38,6 +46,8 @@ func main() {
 		log.Fatal(s.Serve(lis))
 	}()
 
-	err = gateway.Run("dns:///" + addr)
-	log.Fatalln(err)
+	log.Fatalln(http.ListenAndServe(":10001", grpcWebHandler))
+
+	// err = gateway.Run("dns:///" + addr)
+	// log.Fatalln(err)
 }
